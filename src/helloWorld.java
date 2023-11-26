ce8ac94eb888b8114126efb2c77906c43fce58f8
Christopher667 <hellobjtu@gmail.com>
2023-11-26 22:21:48 +0800

    打印“回滚1、2、3”

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -1,6 +1,10 @@
 public class helloWorld {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Successfully Git!");
+        System.out.println("回滚1");
+        System.out.println("回滚2");
+        System.out.println("回滚3");
+
     }
 }
